c650cda477a00bb86ba5f2746440f695c94ecc2c
Sriram Arvind L <lsriramarvind95@hotmail.com>
2016-10-24 12:55:56 +0530

    Service and Complaint Module
    Testing Phase. No view. Just verbose Code.

---
+++ cosmo_base/application/controllers/complaints.php
@@ -0,0 +1,104 @@
+<?php
+class Complaints extends CI_Controller{
+
+  public function index(){
+
+    $data['title'] = 'Complaint Database';
+    $this -> load -> model('complaint');
+
+    $data['complaints'] = $this -> complaint -> GetTicketsC();
+
+    $this -> load -> view ('templates/header', $data);
+    $this -> load -> view ('complaints/index', $data);
+    $this -> load -> view ('templates/footer', $data);
+
+  }
+
+  public function view ($TktNo = NULL)
+  {
+    $this -> load -> model ('complaint');
+    $data['complaint_item'] = $this -> complaint -> GetTicketsC($TktNo);
+
+    if (empty($data['complaint_item']))
+    {
+      show_404();
+    }
+
+    $data['title'] = $data['complaint_item']['ticket_no'];
+
+    $this -> load -> view ('templates/header', $data);
+    $this -> load -> view ('complaints/view', $data);
+    $this -> load -> view ('templates/footer');
+
+
+  }
+
+  public function createticket(){
+
+    $this->load->helper('form');
+    $this->load->helper('url');
+
+      $data['title'] = 'Create new ticket';
+    if(isset($_SESSION['is_loggedin'])&& $_SESSION['is_loggedin'])
+    {
+      if ($_POST)
+      {
+        // Insert Complaint.
+         // Generating ticket number.
+        $TktNo = $_SESSION['userID'];
+        $TktNo .= $this -> input -> post('complaint_id');
+        $TktNo .= date('His', time());
+
+        $complaintData = array(
+          $TktNo,
+          $this -> input -> post('complaint_id'),
+          $this -> input -> post('comment'),
+          1,
+          date('y-m-d', time()),
+          date('H:i:s'),
+          NULL,
+          NULL,
+          $_SESSION['userID']
+        );
+
+        $this -> load -> model('complaint');
+        $val = $this -> complaint -> insert_ticket($complaintData);
+        if ($val['error'] == false)
+        {
+          redirect (base_url() . 'complaints');
+        }
+
+        else
+        {
+          $this -> load -> helper('form');
+          $data ['title'] = 'Complaint';
+          $data ['message'] = $val ['error_message'];
+
+          $this -> load -> view ('templates/header', $data);
+          $this -> load -> view ('complaints/createticket');
+          $this -> load -> view ('templates/footer');
+
+        }
+    }
+    else
+    {
+      // Load form
+      $this -> load -> view ('templates/header', $data);
+      $this -> load -> view ('complaints/createticket', $data);
+      $this -> load -> view ('templates/footer');
+    }
+  }
+    else
+    {
+      redirect (base_url());
+    }
+ }
+
+
+
+
+
+
+}
+
+?>
---
+++ cosmo_base/application/controllers/services.php
@@ -0,0 +1,107 @@
+<?php
+class Services extends CI_Controller
+{
+
+  public function index()
+  {
+    // TODO all the services logged by the users.
+
+    // Load the database and services model for get services.
+    // depending on admin status show the data.
+    $data['title'] = 'services Log';
+
+    $this -> load -> model('service');
+    $data['services'] = $this -> service -> GetTickets();
+    $this -> load -> view ('templates/header', $data);
+    $this -> load -> view ('services/index', $data);
+    $this -> load -> view ('templates/footer', $data);
+
+  }
+
+  public function view ($TktNo = NULL)
+  {
+    $this -> load -> model ('service');
+    $data['service_item'] = $this -> service -> GetTickets($TktNo);
+    if (empty($data['service_item']))
+    {
+      show_404();
+    }
+
+    $data['title'] = $data['service_item']['ticket_no'];
+
+    $this -> load -> view ('templates/header', $data);
+    $this -> load -> view ('services/view', $data);
+    $this -> load -> view ('templates/footer');
+
+
+  }
+  public function createticket()
+  {
+    // Creating a new ticket.
+     $this -> load -> helper ('url');
+     $this -> load -> helper ('form');
+
+    $data['title'] = 'Create new ticket';
+    //echo time();
+    if (isset($_SESSION['is_loggedin']) && $_SESSION['is_loggedin'])
+    {
+      if ($_POST)
+      {
+        // Insert service.
+
+         // Generating ticket number.
+        $TktNo = $_SESSION['userID'];
+        $TktNo .= $this -> input -> post('service_id');
+        $TktNo .= date('His', time());
+
+        $serviceData = array(
+          $TktNo,
+          $this -> input -> post('service_id'),
+          $this -> input -> post('comment'),
+          1,
+          date('y-m-d', time()),
+          date('H:i:s'),
+          NULL,
+          NULL,
+          $_SESSION['userID']
+        );
+
+        $this -> load -> model('service');
+        $val = $this -> service -> insert_ticket($serviceData);
+
+        if ($val['error'] == false)
+        {
+          redirect (base_url() . 'services');
+        }
+
+        else
+        {
+          $this -> load -> helper('form');
+          $data ['title'] = 'service';
+          $data ['message'] = $val ['error_message'];
+
+          $this -> load -> view ('templates/header', $data);
+          $this -> load -> view ('services/createticket');
+          $this -> load -> view ('templates/footer');
+
+        }
+      }
+
+      else
+      {
+        // Load form
+        $this -> load -> view ('templates/header', $data);
+        $this -> load -> view ('services/createticket', $data);
+        $this -> load -> view ('templates/footer');
+
+      }
+    }
+    else
+    {
+      redirect (base_url());
+    }
+    }
+
+
+}
+?>
